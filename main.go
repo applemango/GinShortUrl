28c5d74a57855dca8f83f673f83043aab145c58b
applemango <apple.mangoooo97@gmail.com>
2022-11-08 21:45:02 +0900

    Initial commit

---
+++ main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	engine := gin.Default()
+	ua := ""
+	engine.Use(func(c *gin.Context) {
+		ua = c.GetHeader("User-Agent")
+		c.Next()
+	})
+	engine.GET("/", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"message":    "hello world",
+			"User-Agent": ua,
+		})
+	})
+	engine.Static("/img", "A:/contents_zip/el28whAtuu/")
+	engine.Run(":4000")
+}
---
+++ models/models.go
@@ -0,0 +1,13 @@
+package models
+
+import (
+	"database/sql"
+
+	_ "github.com/mattn/go-sqlite3"
+)
+
+var DB *sql.DB
+
+func main() {
+
+}
