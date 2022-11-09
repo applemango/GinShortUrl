514661e1d4405a4bb3ff89731be80b00d391487e
applemango <apple.mangoooo97@gmail.com>
2022-11-09 11:51:03 +0900

    add db

--- main.go
+++ main.go
@@ -1,24 +1,43 @@
 package main
 
 import (
+	"log"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
+
+	"gin_test/models"
 )
 
+func checkErr(err error) {
+	if err == nil {
+		return
+	}
+	log.Fatal(err)
+}
+
 func main() {
-	engine := gin.Default()
+
+	err := models.ConnectDB()
+	checkErr(err)
+
+	app := gin.Default()
 	ua := ""
-	engine.Use(func(c *gin.Context) {
+	app.Use(func(c *gin.Context) {
 		ua = c.GetHeader("User-Agent")
 		c.Next()
 	})
-	engine.GET("/", func(c *gin.Context) {
+	app.GET("/", func(c *gin.Context) {
 		c.JSON(http.StatusOK, gin.H{
 			"message":    "hello world",
 			"User-Agent": ua,
 		})
 	})
-	engine.Static("/img", "A:/contents_zip/el28whAtuu/")
-	engine.Run(":4000")
+	app.Static("/img", "A:/contents_zip/el28whAtuu/")
+	app.Run(":4000")
 }
+
+/*
+func getUrl(id int) {
+	url, err := models.GetUrl(id)
+}*/
--- models/models.go
+++ models/models.go
@@ -8,6 +8,59 @@ import (
 
 var DB *sql.DB
 
-func main() {
+type Url struct {
+	Id  int    `json:"id"`
+	Url string `json:"url"`
+}
 
+func ConnectDB() error {
+	db, err := sql.Open("sqlite3", "./app.db")
+	if err != nil {
+		return err
+	}
+	query := `CREATE TABLE IF NOT EXISTS url(
+		id INT
+		url STRING
+	)`
+	_, err = db.Exec(query)
+	if err != nil {
+		return err
+	}
+	DB = db
+	return nil
+}
+
+func GetUrl(id int) (Url, error) {
+	stmt, err := DB.Prepare("SELECT id, url from url WHERE id = ?")
+	if err != nil {
+		return Url{}, err
+	}
+	url := Url{}
+	sqlErr := stmt.QueryRow(id).Scan(&url.Id, &url.Url)
+	if sqlErr != nil {
+		if sqlErr == sql.ErrNoRows {
+			return Url{}, nil
+		}
+		return Url{}, sqlErr
+	}
+	return url, nil
+}
+
+func AddUrl(newUrl Url) (bool, error) {
+	tx, err := DB.Begin()
+	if err != nil {
+		return false, err
+	}
+
+	stmt, err := tx.Prepare("INSERT INTO url (id, url) VALUES (?, ?)")
+	if err != nil {
+		return false, err
+	}
+	defer stmt.Close()
+	_, err = stmt.Exec(newUrl.Id, newUrl.Url)
+	if err != nil {
+		return false, err
+	}
+	tx.Commit()
+	return true, nil
 }
