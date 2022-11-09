d9800022b8cef702dc34161ca66ac053b00b7483
applemango <apple.mangoooo97@gmail.com>
2022-11-09 15:43:17 +0900

    add end

--- main.go
+++ main.go
@@ -1,13 +1,20 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
+	"os"
+	"strconv"
 
 	"github.com/gin-gonic/gin"
 
 	"gin_test/models"
 )
+
+type postUrl struct {
+	Url string `form:"url"`
+}
 
 func checkErr(err error) {
 	if err == nil {
@@ -20,9 +27,16 @@ func main() {
 
 	err := models.ConnectDB()
 	checkErr(err)
+	/*add()
+	d, _ := models.GetUrl(1)
+	fmt.Println(d.Url)*/
+
+	d, err := models.GetLastUrl()
+	fmt.Fprintf(os.Stderr, "err:%v\n", err)
+	fmt.Printf("data: %s\n", d.Url)
 
 	app := gin.Default()
-	ua := ""
+	/*ua := ""
 	app.Use(func(c *gin.Context) {
 		ua = c.GetHeader("User-Agent")
 		c.Next()
@@ -33,8 +47,62 @@ func main() {
 			"User-Agent": ua,
 		})
 	})
-	app.Static("/img", "A:/contents_zip/el28whAtuu/")
+	app.Static("/img", "A:/contents_zip/el28whAtuu/")*/
+	app.GET("/url/get", func(c *gin.Context) {
+		id := c.Query("id")
+		intId, err_change := strconv.Atoi(id)
+		data, err_get := models.GetUrl(intId)
+		if id == "" || err_change != nil || err_get != nil {
+			c.JSON(http.StatusOK, gin.H{
+				"id":  -1,
+				"url": "https://example.com",
+			})
+		}
+		c.JSON(http.StatusOK, gin.H{
+			"id":  data.Id,
+			"url": data.Url,
+		})
+	})
+	app.POST("/url/create", func(c *gin.Context) {
+		var u postUrl
+		if c.ShouldBind(&u) != nil {
+			c.JSON(http.StatusOK, gin.H{
+				"id":  -1,
+				"url": "https://example.com",
+			})
+		}
+
+		/*if err != nil {
+			c.JSON(http.StatusOK, gin.H{
+				"id":  -1,
+				"url": "https://example.com",
+			})
+		}*/
+		//var out string
+		//gob.NewDecoder(c.Request.Body).Decode(&out)
+		//fmt.Println(out)
+		add(u.Url)
+		url, _ := models.GetLastUrl()
+		c.JSON(http.StatusOK, gin.H{
+			"id":  url.Id,
+			"url": url.Url,
+		})
+
+	})
 	app.Run(":4000")
+}
+
+func add(url string) bool {
+	var json models.Url
+	json.Url = url
+	//success, err := models.AddUrl(json)
+	success, _ := models.AddUrl(json)
+	if success {
+		return true
+	} else {
+		return false
+		//fmt.Fprintf(os.Stderr, "err :%v\n", err)
+	}
 }
 
 /*
--- models/models.go
+++ models/models.go
@@ -2,6 +2,8 @@ package models
 
 import (
 	"database/sql"
+	"fmt"
+	"os"
 
 	_ "github.com/mattn/go-sqlite3"
 )
@@ -18,12 +20,15 @@ func ConnectDB() error {
 	if err != nil {
 		return err
 	}
-	query := `CREATE TABLE IF NOT EXISTS url(
-		id INT
-		url STRING
-	)`
+	//query := "CREATE TABLE IF NOT EXISTS url ( id int, url string )"
+	query := "CREATE TABLE IF NOT EXISTS url ( id integer primary key autoincrement, url string )"
+	//query := `CREATE TABLE IF NOT EXISTS url(
+	//	id INT
+	//	url STRING
+	//)`
 	_, err = db.Exec(query)
 	if err != nil {
+		fmt.Fprintf(os.Stderr, "err : createDB :%v\n", err)
 		return err
 	}
 	DB = db
@@ -33,6 +38,7 @@ func ConnectDB() error {
 func GetUrl(id int) (Url, error) {
 	stmt, err := DB.Prepare("SELECT id, url from url WHERE id = ?")
 	if err != nil {
+		fmt.Fprintf(os.Stderr, "err : getDB :%v\n", err)
 		return Url{}, err
 	}
 	url := Url{}
@@ -45,6 +51,42 @@ func GetUrl(id int) (Url, error) {
 	}
 	return url, nil
 }
+func GetLastUrl() (Url, error) {
+	stmt, err := DB.Query("SELECT id, url from url WHERE id = last_insert_rowid()")
+	//stmt, err := DB.Query("SELECT id, url from url WHERE id = 1")
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "err : getDB :%v\n", err)
+		return Url{}, err
+	}
+
+	defer stmt.Close()
+	url := Url{}
+
+	//sqlErr := stmt.QueryRow(id).Scan(&url.Id, &url.Url)
+	for stmt.Next() {
+		fmt.Println("f")
+		sqlErr := stmt.Scan(&url.Id, &url.Url)
+		//var id int
+		//var url string
+		//stmt.Scan(&id, &url)
+		if sqlErr != nil {
+			fmt.Fprintf(os.Stderr, "err2: %v\n", err)
+		}
+		//fmt.Println(id)
+		//fmt.Println(url)
+	}
+	//println(url.Id)
+	//println(url.Url)
+	//sqlErr := stmt.Scan(&url.Id, &url.Url)
+	//if sqlErr != nil {
+	//	if sqlErr == sql.ErrNoRows {
+	//		return Url{}, nil
+	//	}
+	//	return Url{}, sqlErr
+	//}
+	return url, nil
+	//return Url{}, nil
+}
 
 func AddUrl(newUrl Url) (bool, error) {
 	tx, err := DB.Begin()
@@ -52,15 +94,21 @@ func AddUrl(newUrl Url) (bool, error) {
 		return false, err
 	}
 
-	stmt, err := tx.Prepare("INSERT INTO url (id, url) VALUES (?, ?)")
+	stmt, err := tx.Prepare("INSERT INTO url ( url ) VALUES ( ? )")
+	//query := "INSERT INTO url ( url ) VALUES ( ? )"
+	//stmt, err := DB.Exec(query, newUrl.Url)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "err: addUrl :%v\n", err)
+		return false, err
+	}
+
+	//defer stmt.Close()
+
+	_, err = stmt.Exec(newUrl.Url)
 	if err != nil {
 		return false, err
 	}
-	defer stmt.Close()
-	_, err = stmt.Exec(newUrl.Id, newUrl.Url)
-	if err != nil {
-		return false, err
-	}
+
 	tx.Commit()
 	return true, nil
 }
